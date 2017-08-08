ab0360731da06de2beaa767e2acdca7891b6fcb9
onojun <snorlax.chemist.and.jazz@gmail.com>
2017-08-08 19:52:31 +0900

    Arduinoのシリアルの整形

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -8,7 +8,7 @@ RCS620S rcs620s;
  
 void setup() { 
   int ret;
-  Serial.begin(115200);      // for RC-S620/S
+  Serial.begin(9600);      // for RC-S620/S
  
   // initialize RC-S620/S
   ret = rcs620s.initDevice();
@@ -19,32 +19,27 @@ void setup() {
                delay(1000);
                // blocking
   }
-
-
 }
  
 void loop() {
   int ret, i;
-
  
   // Polling
   rcs620s.timeout = COMMAND_TIMEOUT;
   ret = rcs620s.polling();
   
   if(ret) {
-    Serial.print("IDm:");
     for(i = 0; i < 8; i++)
     {
-      if(rcs620s.idm[i] / 0x10 == 0) Serial.print(0);
-      Serial.print(rcs620s.idm[i], HEX);
+      Serial.println(rcs620s.idm[i], HEX);
     }
 
-            tone(9,1318,80);
-  delay(100);
-  tone(9,1318,80);
-  delay(5000);
+    tone(9,1318,80);
+    delay(100);
+    tone(9,1318,80);
+    delay(5000);
   } else {
-    Serial.print("Polling...");
+    Serial.println("Polling...");
   }
  
   rcs620s.rfOff();
