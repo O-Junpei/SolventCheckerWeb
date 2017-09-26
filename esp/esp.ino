ac72b8fa378d4f3e643a70fba51cb8868227a4ea
onojun <snorlax.chemist.and.jazz@gmail.com>
2017-09-26 21:57:52 +0900

    少しリファクタリング 色々書き込めるようにした。

--- esp/esp.ino
+++ esp/esp.ino
@@ -5,7 +5,6 @@
 //フェリカ関係
 #define COMMAND_TIMEOUT 400
 #define POLLING_INTERVAL 500
-
 RCS620S rcs620s;
 
 //ブザー関連
@@ -19,28 +18,26 @@ HX711 scale(14, 15);
 
 //SD関係
 #define LOG_FILE_NAME "/log.txt"
-const uint8_t SD_CS = 5; // GPIO5=CS
+#define SD_CS 5
 static File s_myFile;
-
 
 void setup() { 
 
-  int ret;
   Serial.begin(115200);      // for RC-S620/S
 
-  // initialize RC-S620/S
+  //felica
+  int ret;
   ret = rcs620s.initDevice();
 
   // 音を鳴らす準備
   ledcSetup(LEDC_CHANNEL, LEDC_BASE_FREQ, LEDC_TIMER_BIT);
   ledcAttachPin(BUZZER_PIN, LEDC_CHANNEL);
+  //callZeldaSound();
 
-  callZeldaSound();
-  Serial.println("kokokara");
-
+  //SDの確認
   SD_init();
-  SD_write();
-  Serial.println(SD_read());
+  //SD_write("17/09/32","idididid", "32567");
+  //Serial.println(SD_read());
 
   while (!ret) {
       ret = rcs620s.initDevice();
@@ -50,55 +47,6 @@ void setup() {
   }
 }
 
-
-void SD_init()
-{
-    Serial.print("Initializing SD card...");
-
-    if (!SD.begin(SD_CS)) {
-      Serial.println("initialization failed!");
-      return;
-    }
-    Serial.println("initialization done.");        
-}
-
-String SD_read() {
-
-    String str;
-
-    File file = SD.open(LOG_FILE_NAME, FILE_READ);
-
-    if(file){
-        //---1byteずつ読み込んだ文字を結合
-        while (file.available()) {
-            str += char(file.read());
-        }
-    } else{
-        Serial.println(" error...");
-    }
-    //---ファイルを閉じる
-    file.close();
-    
-    return str;
-}
-
-void SD_write()
-{
-    String backLog = SD_read();
-
-    s_myFile = SD.open(LOG_FILE_NAME, FILE_WRITE);
-    if (s_myFile) {
-        Serial.print("Writing to test.txt...");
-        s_myFile.print(backLog);
-        s_myFile.println("testing 1, 2, 3.");
-        s_myFile.close();
-        Serial.println("done.");
-    } else {
-        Serial.println("error opening test.txt");
-    }
-}
-
-
 void loop() {
   int ret, i;
  
@@ -107,13 +55,11 @@ void loop() {
   ret = rcs620s.polling();
   
   if(ret) {
-    Serial.print(" ");
     for(i = 0; i < 8; i++)
     {
       if(rcs620s.idm[i] / 0x10 == 0) Serial.print(0);
       Serial.print(rcs620s.idm[i], HEX);
     }
-    Serial.print(" ");
     Serial.println();
 
     callZeldaSound();
